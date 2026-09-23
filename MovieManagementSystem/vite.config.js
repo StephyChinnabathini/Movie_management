7ea11a3b57ed08326c008af9727d9286942252bb
stephychinnabathini-coder <stephychinnabathini@gmail.com>
2026-09-23 10:25:18 +0530

    Fix Render allowed host

--- MovieManagementSystem/vite.config.js
+++ MovieManagementSystem/vite.config.js
@@ -4,4 +4,8 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  preview: {
+    allowedHosts: ['movie-management-frontend-e8yx.onrender.com']
+  }
 })
